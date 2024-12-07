ae10a95116fbcf4c8b8a9820e048514deb6afbcd
Puneet0910 <nainwalpuneet@gmail.com>
2024-12-07 13:39:36 +0530

    Created the backend api of the login

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,3 +16,20 @@ exports.signup = async (req,res,next)=>{
         return res.status(500).json({message:"Internal Server error"});
     }
 };
+exports.login = async (req,res,next)=>{
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({where:{email}});
+        if(!user){
+            return res.status(404).json({message:"User Not Found"});
+        };
+        if(user.password !== password){
+            return res.status(401).json({message:"Invalid Password"});
+        };
+        return res.status(200).json({message:"Login Successfull", user});
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({message:"Internal Server error"});
+    }
+};
---
+++ login.js
@@ -0,0 +1,19 @@
+async   function login(event) {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const user = { email, password };
+
+    try {
+        const response = await axios.post("http://localhost:3000/user/login", user);
+        console.log("Response:", response.data);
+        alert("Login successful!")
+    } catch (error) {
+        if (error.response) {
+            alert(error.response.data.message);
+        } else {
+            console.error(error);
+            alert("An unexpected error occurred. Please try again.");
+        }
+    }
+}
